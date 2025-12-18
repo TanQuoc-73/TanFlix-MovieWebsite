64dc0863661724800a710ef70fc6d1575816bd12
TanQuoc-73 <cutorasusu2004@Gmail.com>
2025-12-18 15:56:22 +0700

    fe

---
+++ frontend/components/layouts/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='flex flex-col h-20 w-full bg-white shadow-md justify-center items-center'>
+      
+    </div>
+  )
+}
